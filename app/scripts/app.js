f93dd7fc81ce4e92b0d58bcbcb44313c08ad9371
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-10-16 22:50:15 -0700

    Initial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoTestApp
+ * @description
+ * # yoTestApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('yoTestApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'MainCtrl',
+        controllerAs: 'contact'
+      })
+      .when('/video', {
+        templateUrl: 'views/video.html',
+        controller: 'MainCtrl',
+        controllerAs: 'video'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
